9d750cfeec381b733dd187166797a7d8e52ac424
Fernando Gurgel <gurgelff@gmail.com>
2024-02-09 17:16:31 -0300

    feat(app.ts): use routes for organization

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import helmet from "helmet";
+import routes from "./routes";
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  return res.json({ status: "ok" });
-});
+app.use(routes);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server started on port ${process.env.SERVER_PORT}`);
