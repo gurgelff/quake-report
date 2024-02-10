02f3f1aa240a4027da2c5b2d5c1720466989bfa5
Fernando Gurgel <gurgelff@gmail.com>
2024-02-10 00:59:49 -0300

    feat(app): use invalidRequestHandler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import helmet from "helmet";
 import routes from "./routes";
+import invalidRequestHandler from "@middlewares/invalidRequestHandler";
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
+app.use(invalidRequestHandler);
 app.use(routes);
 
 app.listen(process.env.SERVER_PORT, () => {
